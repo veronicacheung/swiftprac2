f7cb930f3a87b8050441b6e17cb9516705bb56b0
Veronica Cheung <veronica.cheung1105@gmail.com>
2021-07-28 12:01:53 -0700

    Initial Commit

---
+++ swiftprac2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swiftprac2
+//
+//  Created by Veronica Cheung on 7/28/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
