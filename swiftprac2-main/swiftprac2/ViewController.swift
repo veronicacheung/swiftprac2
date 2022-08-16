97481e1eb7b7f2b145a88f2c859a9edf72f172e4
Veronica Cheung <30807219+veronicacheung@users.noreply.github.com>
2022-08-16 11:19:35 -0400

    Add random label

---
+++ swiftprac2-main/swiftprac2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swiftprac2
+//
+//  Created by Veronica Cheung on 7/28/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
